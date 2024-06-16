f41ab0ef2e8b126f6a80b05ea38a088f480d4f03
Ishu Kumari Jasrajpuria <ishukumarijasrajpuria@Ishus-Laptop.local>
2024-06-16 01:09:20 -0700

    add style to modal

--- src/Components/CopModal.js
+++ src/Components/CopModal.js
@@ -44,27 +44,33 @@ function CopModal({ cop, oncloseModal, onDone, updateCopInfo, cities, vehicles, 
     };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-            <div className="bg-white p-8 rounded-lg shadow-2xl z-60 max-w-8xl w-full">
-                <div className='text-3xl font-bold mb-10'>Select a city and a vehicle</div>
-                <div className='flex justify-center space-x-6 mb-8'>
-                    {cities.map((city) => (
-                        <div key={city.id} className={`cursor-pointer ${city.selected ? 'opacity-50' : ''}`} onClick={() => handleCityClick(city)}>
-                            <img src={city.image} alt={`city${city.id}`} className={`h-80 w-80 object-cover rounded-lg ${selectedCity === city ? 'border-8 border-green-500 shadow-lg transform scale-105' : ''}`} />
-                        </div>
-                    ))}
+        <div className="fixed inset-0 flex items-center justify-center bg-dark-blue bg-opacity-75 z-50">
+            <div className="bg-gray-800 p-8 rounded-lg shadow-2xl z-60 max-w-8xl w-full">
+                <div className='text-3xl font-bold mb-10 text-white'>Choose your mission parameters</div>
+                <div className='mb-8'>
+                    <div className='text-2xl font-semibold text-white mb-4'>Select a City</div>
+                    <div className='flex justify-center space-x-6'>
+                        {cities.map((city) => (
+                            <div key={city.id} className={`cursor-pointer ${city.selected ? 'opacity-50' : ''}`} onClick={() => handleCityClick(city)}>
+                                <img src={city.image} alt={`city${city.id}`} className={`h-80 w-80 object-cover rounded-lg transition-transform duration-300 ${selectedCity === city ? 'border-8 border-green-500 shadow-lg transform scale-105' : 'hover:scale-105 hover:shadow-lg'}`} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className='flex justify-center space-x-6 mb-8'>
-                    {vehicles.map((vehicle) => (
-                        <div key={vehicle.id} className={`cursor-pointer ${vehicle.selected || vehicle.count <= 0 ? 'opacity-50' : ''}`} onClick={() => handleVehicleClick(vehicle)}>
-                            <img src={vehicle.image} alt={`vehicle${vehicle.id}`} className={`h-80 w-80 object-cover rounded-lg ${selectedVehicle === vehicle ? 'border-8 border-blue-500 shadow-lg transform scale-105' : ''}`} />
-                        </div>
-                    ))}
+                <div className='mb-8'>
+                    <div className='text-2xl font-semibold text-white mb-4'>Select a Vehicle</div>
+                    <div className='flex justify-center space-x-6'>
+                        {vehicles.map((vehicle) => (
+                            <div key={vehicle.id} className={`cursor-pointer ${vehicle.selected || vehicle.count <= 0 ? 'opacity-50' : ''}`} onClick={() => handleVehicleClick(vehicle)}>
+                                <img src={vehicle.image} alt={`vehicle${vehicle.id}`} className={`h-80 w-80 object-cover rounded-lg transition-transform duration-300 ${selectedVehicle === vehicle ? 'border-8 border-blue-500 shadow-lg transform scale-105' : 'hover:scale-105 hover:shadow-lg'}`} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
                 <div className="flex justify-center space-x-4 mt-8">
                     <button
                         onClick={handleDoneClick}
-                        className="bg-blue-500 text-white px-8 py-4 rounded-full text-lg font-semibold shadow-lg hover:shadow-xl transform transition-transform duration-300 hover:scale-105"
+                        className="bg-blue-700 text-white px-8 py-4 rounded-full text-lg font-semibold shadow-lg hover:shadow-xl transform transition-transform duration-300 hover:scale-105"
                     >
                         Done
                     </button>
